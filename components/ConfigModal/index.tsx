09410850eb81f556ad07171d38f0b681f54f70fb
rony646 <rony646@users.noreply.github.com>
2021-04-15 21:30:24 -0300

    adding content to modal

--- components/ConfigModal/index.tsx
+++ components/ConfigModal/index.tsx
@@ -1,7 +1,9 @@
-import React, {} from 'react';
+import React, { useState } from 'react';
 
 import { AntDesign } from '@expo/vector-icons'
 import { View, Text, Modal, StyleSheet, Button, Pressable } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+import Colors from '../../constants/Colors';
 
 interface ConfigModalProps {
     isOpen: boolean;
@@ -9,6 +11,78 @@ interface ConfigModalProps {
 }
 
 const ConfigModal: React.FC<ConfigModalProps> = ({isOpen, closeModal}) => {
+
+    const [colors, setColors] = useState({
+        color1: {
+            colorCode: Colors.themePrimaryColor,
+            selected: true,
+        },
+        color2: {
+            colorCode: Colors.themeSecondaryColor,
+            selected: false,
+        },
+        color3: {
+            colorCode: Colors.themeTertiaryColor,
+            selected: false,
+        }
+    })
+
+    const onChangeColorHandler = (color: string) => {
+        switch(color) {
+            case 'color1':
+                let newColors = {
+                    color1: {
+                        colorCode: Colors.themePrimaryColor,
+                        selected: true,
+                    },
+                    color2: {
+                        colorCode: Colors.themeSecondaryColor,
+                        selected: false,
+                    },
+                    color3: {
+                        colorCode: Colors.themeTertiaryColor,
+                        selected: false,
+                    }
+                };
+                setColors(newColors);
+                break;
+            case 'color2':
+                newColors = {
+                    color1: {
+                        colorCode: Colors.themePrimaryColor,
+                        selected: false,
+                    },
+                    color2: {
+                        colorCode: Colors.themeSecondaryColor,
+                        selected: true,
+                    },
+                    color3: {
+                        colorCode: Colors.themeTertiaryColor,
+                        selected: false,
+                    }
+                };
+                setColors(newColors);
+                break;
+            case 'color3':
+                newColors = {
+                    color1: {
+                        colorCode: Colors.themePrimaryColor,
+                        selected: false,
+                    },
+                    color2: {
+                        colorCode: Colors.themeSecondaryColor,
+                        selected: false,
+                    },
+                    color3: {
+                        colorCode: Colors.themeTertiaryColor,
+                        selected: true,
+                    }
+                };
+                setColors(newColors);
+                break;
+        }
+    };
+
     return(
         <Modal
             animationType="slide"
@@ -19,6 +93,25 @@ const ConfigModal: React.FC<ConfigModalProps> = ({isOpen, closeModal}) => {
                 <Pressable style={styles.closeButton} onPress={closeModal}>
                     <AntDesign name="close" size={40} color="black"/>
                 </Pressable>
+                <View style={styles.colorOptionsContainer}>
+                    <Text style={styles.title}>Color Settings</Text>
+                    <Text>Select a theme color to the app: </Text>
+                </View>
+                <Text>Color 1</Text>
+                <CheckBox
+                    value={colors.color1.selected}
+                    onValueChange={() => onChangeColorHandler('color1')}
+                />
+                 <Text>Color 2</Text>
+                 <CheckBox
+                    value={colors.color2.selected}
+                    onValueChange={() => onChangeColorHandler('color2')}
+                />
+                 <Text>Color 3</Text>
+                 <CheckBox
+                    value={colors.color3.selected}
+                    onValueChange={() => onChangeColorHandler('color3')}
+                />
             </View>
 
         </Modal>
@@ -38,6 +131,15 @@ const styles = StyleSheet.create({
     closeButton: {
         marginLeft: 20,
         marginTop: 10
+    },
+    colorOptionsContainer: {
+        height: '40%',
+        // backgroundColor: 'red',
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold'
     }
 })
 
--- constants/Colors.tsx
+++ constants/Colors.tsx
@@ -2,6 +2,8 @@ const colors = {
     primary1: '#272956',
     primary2: '#1b2040',
     themePrimaryColor: '#f47375',
+    themeSecondaryColor: '#25cfdb',
+    themeTertiaryColor: '#8f03ab'
 };
 
 export default colors;
